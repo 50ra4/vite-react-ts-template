af736738267cb38be777bcb72541254c23a82311
50ra4 <50ra4.dev.dev@gmail.com>
2022-10-16 00:22:39 +0900

    fix: SampleComponent import path

--- src/presentation/components/SampleComponent/SampleComponent.test.tsx
+++ src/presentation/components/SampleComponent/SampleComponent.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
 import '@testing-library/jest-dom';
-import { SampleComponent } from '@/components/SampleComponent';
+import { SampleComponent } from '@/components/SampleComponent/SampleComponent';
 
 describe('SampleComponent.tsx', () => {
   test('render SampleComponent', () => {
