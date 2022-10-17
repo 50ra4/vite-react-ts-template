45e5d83ace95e71d142e4babb06ac0e76d879f33
50ra4 <50ra4.dev.dev@gmail.com>
2022-10-17 22:48:47 +0900

    add: pages component sample

---
+++ src/presentation/pages/TopPage/TopPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function TopPage() {
+  return (
+    <main>
+      <h2>TopPage</h2>
+      <div>TopPage component</div>
+    </main>
+  );
+}
+
+export default TopPage;
