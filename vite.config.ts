31762c02b5d25eff64b3235447b931dda05a451c
50ra4 <50ra4.dev.dev@gmail.com>
2022-10-19 01:15:29 +0900

    fix: base path setting for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // for deploy to sub directory
+  base: process.env.BASE_PATH,
   resolve: {
     alias: [
       { find: '@/components', replacement: '/src/presentation/components' },
