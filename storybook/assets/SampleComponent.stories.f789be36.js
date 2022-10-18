9a9fd8889b22b5881904dac29933d2b6ccb3fb27
50ra4 <50ra4@users.noreply.github.com>
2022-10-18 15:54:41 +0000

    deploy: fc29d54273a343bfe0b2fc22fc4f6e841b691f67

---
+++ storybook/assets/SampleComponent.stories.f789be36.js
@@ -0,0 +1,2 @@
+var t=Object.defineProperty;var o=(e,n)=>t(e,"name",{value:n,configurable:!0});import{j as m}from"./jsx-runtime.f75e1641.js";import"./iframe.543eb6c5.js";const p=o(()=>m("div",{children:"Sample Component"}),"SampleComponent"),l={component:p},c={args:{}},d=["Example"];export{c as Example,d as __namedExportsOrder,l as default};
+//# sourceMappingURL=SampleComponent.stories.f789be36.js.map
