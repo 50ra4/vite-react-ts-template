1204d9c853d9595dd7d65df6274c51d622d99f3d
50ra4 <50ra4.dev.dev@gmail.com>
2022-10-19 09:39:10 +0900

    fix: ErrorPage link

--- src/presentation/pages/ErrorPage/ErrorPage.tsx
+++ src/presentation/pages/ErrorPage/ErrorPage.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function ErrorPage() {
   return (
     <main>
       <h2>Error Page</h2>
-      <a href="/">TopPage</a>
+      <Link to="/">TopPage</Link>
     </main>
   );
 }
