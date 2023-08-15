603080c7328e95620399775fafd8597d3d111a29
50ra4 <50ra4@users.noreply.github.com>
2023-08-15 09:43:07 +0000

    deploy: 4244bf783e6847a0fbcfefff8438272ec6ad35de

--- assets/Page2-59ca2be3.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-9e9254a3.js";function r(){return e.jsxs("main",{children:[e.jsx("h2",{children:"Page2"}),e.jsx("div",{children:"Page2 component"})]})}export{r as default};
--- assets/Page3-94424090.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-9e9254a3.js";function r(){return e.jsxs("main",{children:[e.jsx("h2",{children:"Page3"}),e.jsx("div",{children:"Page3 component"})]})}export{r as default};
