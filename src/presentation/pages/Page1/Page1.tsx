898991f992ba70a08f2c229feb707712b950d8e6
50ra4 <50ra4.dev.dev@gmail.com>
2023-08-15 18:40:44 +0900

    chore: add error boundary sample

--- src/presentation/pages/Page1/Page1.tsx
+++ src/presentation/pages/Page1/Page1.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { useThrowError } from 'src/hooks/useThrowError';
 
 function Page1() {
+  const { throwError } = useThrowError();
+
   return (
     <main>
       <h2>Page1</h2>
+      <button
+        onClick={() => {
+          throwError(new Error('oops!'));
+        }}
+      ></button>
       <div>Page1 component</div>
     </main>
   );
